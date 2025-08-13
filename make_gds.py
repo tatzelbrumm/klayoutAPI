427b89e05c85c2620ff1e7efb78de80f365d81ed
Christoph Maier <christoph.maier@ieee.org>
2025-08-13 03:42:59 +0200

    make_gds.py : add width variable, set to 400nm

--- make_gds.py
+++ make_gds.py
@@ -6,6 +6,10 @@ layout = pya.Layout()
 layout.dbu = 0.001  # 1 nm units
 top = layout.create_cell("SWITCHED_PMOS_CASCODE")
 
+# Parameters
+l = 300
+w = 400
+
 # Layers
 l_active = layout.layer(pya.LayerInfo(1, 0))
 l_po     = layout.layer(pya.LayerInfo(5, 0))
@@ -15,12 +19,12 @@ l_pimp   = layout.layer(pya.LayerInfo(14, 0))
 l_nwell  = layout.layer(pya.LayerInfo(31, 0))
 
 # Draw shapes
-top.shapes(l_active).insert(pya.Box(290, -380, 440, 380))
-top.shapes(l_po).insert(    pya.Box(-150, -150, 620, 150))
+top.shapes(l_active).insert(pya.Box(290, -380, 290+w, 380))
+top.shapes(l_po).insert(    pya.Box(-150, -150, 470+w, 150))
 top.shapes(l_co).insert(    pya.Box(-80,  -80,   80,  80))
 top.shapes(l_m1).insert(    pya.Box(-80, -130,   80, 130))
-top.shapes(l_pimp).insert(  pya.Box(-10, -560,  740, 560))
-top.shapes(l_nwell).insert( pya.Box(-30, -790,  760, 790))
+top.shapes(l_pimp).insert(  pya.Box(-10, -560,  590+w, 560))
+top.shapes(l_nwell).insert( pya.Box(-30, -790,  610+w, 790))
 
 # Save GDS
 layout.write("switched_pmos_cascode.gds")
