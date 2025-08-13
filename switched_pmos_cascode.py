8097a9628310d651b9c5db753f084331adf4561f
Christoph Maier <christoph.maier@ieee.org>
2025-08-13 16:15:14 +0200

    switched_pmos_cascode.py : works after .lym conversion into .klayout/pymacros

--- switched_pmos_cascode.py
+++ switched_pmos_cascode.py
@@ -7,22 +7,23 @@ class SwitchedPMOSCascode(pya.PCellDeclarationHelper):
     def __init__(self):
         super().__init__()
         # Parameters
-        self.param("l", self.TypeDouble, "cascode length", default=0.4)
-        self.param("w", self.TypeDouble, "cascode width",  default=0.3)
+        self.param("l", self.TypeInt, "cascode length (nm)", default=400)
+        self.param("w", self.TypeInt, "cascode width (nm)",  default=300)
         self.param("ly_active", self.TypeLayer, "active area (diffusion)", default=pya.LayerInfo(1, 0))
         self.param("ly_po",     self.TypeLayer, "Poly (Gate)",             default=pya.LayerInfo(5, 0))
         self.param("ly_co",     self.TypeLayer, "Contact (CO)",            default=pya.LayerInfo(6, 0))
         self.param("ly_m1",     self.TypeLayer, "Metal1 (M1)",             default=pya.LayerInfo(8, 0))
         self.param("ly_pimp",   self.TypeLayer, "P+ implant",              default=pya.LayerInfo(14, 0))
         self.param("ly_nwell",  self.TypeLayer, "N-Well",                  default=pya.LayerInfo(31, 0))
+        self.param("ly_pr",     self.TypeLayer, "Placement boundary",      default=pya.LayerInfo(63, 0))
 
     def display_text_impl(self):
         return f"Switched PMOS cascode_W{self.w}_L{self.l}"
 
     def coerce_parameters_impl(self):
         # Keep parameters valid; add rules as needed
-        if self.l <= 0: self.l = 0.1
-        if self.w <= 0: self.w = 0.1
+        if self.l <= 130: self.l = 130
+        if self.w <= 130: self.w = 130
 
     def produce_impl(self):
         # Resolve layers
@@ -32,15 +33,19 @@ class SwitchedPMOSCascode(pya.PCellDeclarationHelper):
         ly_m1     = self.layout.layer(self.ly_m1)
         ly_pimp   = self.layout.layer(self.ly_pimp)
         ly_nwell  = self.layout.layer(self.ly_nwell)
+        ly_pr     = self.layout.layer(self.ly_pr)
 
         shapes = self.cell.shapes
+        lhalf=self.l//2
+        w=self.w
         # Draw some simple geometry (database units)
-        shapes(ly_active).insert(pya.Box(290, -380, 440, 380))
-        shapes(ly_po).insert(    pya.Box(-150, -150, 620, 150))
+        shapes(ly_active).insert(pya.Box(290, -(lhalf+230), 290+w, (lhalf+230)))
+        shapes(ly_po).insert(    pya.Box(-150, -lhalf, 470+w, lhalf))
         shapes(ly_co).insert(    pya.Box(-80,  -80,   80,  80))
         shapes(ly_m1).insert(    pya.Box(-80, -130,   80, 130))
-        shapes(ly_pimp).insert(  pya.Box(-10, -560,  740, 560))
-        shapes(ly_nwell).insert( pya.Box(-30, -790,  760, 790))
+        shapes(ly_pimp).insert(  pya.Box(-10, -(lhalf+410),  590+w, (lhalf+410)))
+        shapes(ly_nwell).insert( pya.Box(-30, -(lhalf+640),  610+w, (lhalf+640)))
+        shapes(ly_pr).insert(    pya.Box(-190, -(lhalf+640),  610+w, (lhalf+640)))
 
 class PMOSSourcesLib(pya.Library):
     def __init__(self):
