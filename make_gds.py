de618d1ebbfd117f56fec6da972d17d496d6ef65
Christoph Maier <christoph.maier@ieee.org>
2025-08-13 03:59:30 +0200

    make_gds.py switched_pmos_cascode.gds : 0.4/0.2, poly contact DRC error

--- make_gds.py
+++ make_gds.py
@@ -7,7 +7,7 @@ layout.dbu = 0.001  # 1 nm units
 top = layout.create_cell("SWITCHED_PMOS_CASCODE")
 
 # Parameters
-l = 300
+l = 200
 w = 400
 
 # Layers
@@ -19,12 +19,12 @@ l_pimp   = layout.layer(pya.LayerInfo(14, 0))
 l_nwell  = layout.layer(pya.LayerInfo(31, 0))
 
 # Draw shapes
-top.shapes(l_active).insert(pya.Box(290, -380, 290+w, 380))
-top.shapes(l_po).insert(    pya.Box(-150, -150, 470+w, 150))
+top.shapes(l_active).insert(pya.Box(290, -(l/2+230), 290+w, (l/2+230)))
+top.shapes(l_po).insert(    pya.Box(-150, -l/2, 470+w, l/2))
 top.shapes(l_co).insert(    pya.Box(-80,  -80,   80,  80))
 top.shapes(l_m1).insert(    pya.Box(-80, -130,   80, 130))
-top.shapes(l_pimp).insert(  pya.Box(-10, -560,  590+w, 560))
-top.shapes(l_nwell).insert( pya.Box(-30, -790,  610+w, 790))
+top.shapes(l_pimp).insert(  pya.Box(-10, -(l/2+410),  590+w, (l/2+410)))
+top.shapes(l_nwell).insert( pya.Box(-30, -(l/2+640),  610+w, (l/2+640)))
 
 # Save GDS
 layout.write("switched_pmos_cascode.gds")
