0ae5c5b721c702fc056ef29167cb485ecdd3db27
Christoph Maier <christoph.maier@ieee.org>
2025-08-29 15:02:53 +0200

    make_dac_pads.py dac_pads.gds : centered text

--- make_dac_pads.py
+++ make_dac_pads.py
@@ -10,17 +10,24 @@ layout.dbu = 0.001  # 1 nm units
 top = layout.create_cell("SWITCHED_PMOS_CASCODE")
 
 # Parameters
-n = 300
+n = [1, 64, 1]
+label = ["EN","ON","EN"]
 
 # Layers
-l_m2     = layout.layer(pya.LayerInfo(10, 0))
-l_m2text = layout.layer(pya.LayerInfo(10, 2))
+l_m2    = layout.layer(pya.LayerInfo(10, 0))
+l_m2pin = layout.layer(pya.LayerInfo(10, 2))
 
 # Draw shapes
-top.shapes(l_m2).insert(    pya.Box( -420, -4910,  -130, -4620))
-top.shapes(l_m2).insert(    pya.Box(  130, -4910,   420, -4620))
-top.shapes(l_m2).insert(    pya.Text("ON", -275, -4765))
-top.shapes(l_m2).insert(    pya.Text("ONB", 275, -4765))
+top.shapes(l_m2pin).insert(    pya.Box( -420, -4910,  -130, -4620))
+top.shapes(l_m2pin).insert(    pya.Box(  130, -4910,   420, -4620))
+text= pya.Text("ON", -275, -4765)
+text.halign = pya.Text.HAlignCenter
+text.valign = pya.Text.VAlignCenter
+top.shapes(l_m2pin).insert(text)
+text= pya.Text("ONB",  275, -4765)
+text.halign = pya.Text.HAlignCenter
+text.valign = pya.Text.VAlignCenter
+top.shapes(l_m2pin).insert(text)
 
 # Save GDS
 layout.write("dac_pads.gds")
